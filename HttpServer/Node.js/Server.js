15d8d797b30b04dfd12bdb65484d41d393fb3df3
guanvee <guanvee@qq.com>
2019-12-13 15:46:01 +0800

    Add url output

--- HttpServer/Node.js/Server.js
+++ HttpServer/Node.js/Server.js
@@ -8,12 +8,13 @@ var server = http.createServer(function(req, res) {
         body = Buffer.concat(body).toString();
         res.writeHead(200, { 'Content-Type': 'text/json' });
         res.end('{"Successful":true,"Message":"Upload successful."}\n');
-        console.log('Got data:' + body);
+        console.log('Upload data: ' + body);
     });
+    console.log('Got url request: ' + req.url);
 });
-// server.on('clientError', (err, socket) => {
-//     console.log(err);
-//     socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
-// });
-server.listen(80);
-console.log('Server running at http://127.0.0.1:80/');
+server.on('clientError', (err, socket) => {
+    console.log(err);
+    socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
+});
+server.listen(8000);
+console.log('Server running at http://127.0.0.1:8000/');
