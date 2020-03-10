268d58cd6256f2b2f71fe9617e112ab746c2ae46
lewei50 <service@lewei50.com>
2020-03-10 13:24:03 +0800

    Add files via upload

--- homeassistant/custom_components/iammeter/sensor.py
+++ homeassistant/custom_components/iammeter/sensor.py
@@ -1,9 +1,9 @@
 """Support for iammeter via local API."""
 import asyncio
-import async_timeout
 from datetime import timedelta
 import logging
 
+import async_timeout
 from iammeter import real_time_api
 from iammeter.power_meter import IamMeterError
 import voluptuous as vol
@@ -11,9 +11,10 @@ import voluptuous as vol
 from homeassistant.components.sensor import PLATFORM_SCHEMA
 from homeassistant.const import CONF_HOST, CONF_NAME, CONF_PORT
 from homeassistant.exceptions import PlatformNotReady
+from homeassistant.helpers import debounce
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.entity import Entity
-from homeassistant.helpers.event import async_track_time_interval
+from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -34,68 +35,57 @@ PLATFORM_TIMEOUT = 8
 
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
     """Platform setup."""
+    config_host = config[CONF_HOST]
+    config_port = config[CONF_PORT]
+    config_name = config[CONF_NAME]
     try:
         with async_timeout.timeout(PLATFORM_TIMEOUT):
-            api = await real_time_api(config[CONF_HOST], config[CONF_PORT])
-    except asyncio.TimeoutError:
-        _LOGGER.error(f"IamMeter device {config[CONF_NAME]} not ready.")
+            api = await real_time_api(config_host, config_port)
+    except (IamMeterError, asyncio.TimeoutError):
+        _LOGGER.error("Device is not ready")
         raise PlatformNotReady
-    endpoint = RealTimeDataEndpoint(hass, api)
-    devices = []
-    for sensor, (row, idx, unit) in api.iammeter.sensor_map().items():
-        uid = f"{config[CONF_NAME]}-{api.iammeter.mac}-{api.iammeter.serial_number}-{row}-{idx}"
-        devices.append(
-            IamMeter(uid, api.iammeter.serial_number, sensor, unit, config[CONF_NAME])
-        )
-    endpoint.sensors = devices
-    async_add_entities(devices)
-    async_track_time_interval(hass, endpoint.async_refresh, SCAN_INTERVAL)
 
-
-class RealTimeDataEndpoint:
-    """Representation of a Sensor."""
-
-    def __init__(self, hass, api):
-        """Initialize the sensor."""
-        self.hass = hass
-        self.api = api
-        self.ready = asyncio.Event()
-        self.sensors = []
-
-    async def async_refresh(self, now=None):
-        """Fetch new state data for the sensor.This is the only method that should fetch new data for Home Assistant."""
+    async def async_update_data():
         try:
-            api_response = await self.api.get_data()
-        except IamMeterError:
-            if now is not None:
-                self.ready.clear()
-                _LOGGER.error("data refresh timeout.")
-                return
-            raise PlatformNotReady
-        self.ready.set()
-        data = api_response.data
-        for sensor in self.sensors:
-            if sensor.key in data:
-                sensor.value = data[sensor.key]
-                sensor.schedule_update_ha_state()
+            with async_timeout.timeout(PLATFORM_TIMEOUT):
+                return await api.get_data()
+        except (IamMeterError, asyncio.TimeoutError):
+            raise UpdateFailed
+
+    coordinator = DataUpdateCoordinator(
+        hass,
+        _LOGGER,
+        name=DEFAULT_DEVICE_NAME,
+        update_method=async_update_data,
+        update_interval=SCAN_INTERVAL,
+        request_refresh_debouncer=debounce.Debouncer(
+            hass, _LOGGER, cooldown=0.3, immediate=True
+        ),
+    )
+    await coordinator.async_refresh()
+    entities = []
+    for sensor_name, (row, idx, unit) in api.iammeter.sensor_map().items():
+        serial_number = api.iammeter.serial_number
+        uid = f"{serial_number}-{row}-{idx}"
+        entities.append(IamMeter(coordinator, uid, sensor_name, unit, config_name))
+    async_add_entities(entities)
 
 
 class IamMeter(Entity):
     """Class for a sensor."""
 
-    def __init__(self, uid, serial, key, unit, dev_name):
+    def __init__(self, coordinator, uid, sensor_name, unit, dev_name):
         """Initialize an iammeter sensor."""
+        self.coordinator = coordinator
         self.uid = uid
-        self.serial = serial
-        self.key = key
-        self.value = None
+        self.sensor_name = sensor_name
         self.unit = unit
         self.dev_name = dev_name
 
     @property
     def state(self):
-        """State of this iammeter attribute."""
-        return self.value
+        """Return the state of the sensor."""
+        return self.coordinator.data.data[self.sensor_name]
 
     @property
     def unique_id(self):
@@ -105,7 +95,7 @@ class IamMeter(Entity):
     @property
     def name(self):
         """Name of this iammeter attribute."""
-        return f"{self.dev_name} {self.key}"
+        return f"{self.dev_name} {self.sensor_name}"
 
     @property
     def icon(self):
@@ -119,5 +109,22 @@ class IamMeter(Entity):
 
     @property
     def should_poll(self):
-        """No polling needed."""
+        """Poll needed."""
         return False
+
+    @property
+    def available(self):
+        """Return if entity is available."""
+        return self.coordinator.last_update_success
+
+    async def async_added_to_hass(self):
+        """When entity is added to hass."""
+        self.coordinator.async_add_listener(self.async_write_ha_state)
+
+    async def async_will_remove_from_hass(self):
+        """When entity will be removed from hass."""
+        self.coordinator.async_remove_listener(self.async_write_ha_state)
+
+    async def async_update(self):
+        """Update the entity."""
+        await self.coordinator.async_request_refresh()
